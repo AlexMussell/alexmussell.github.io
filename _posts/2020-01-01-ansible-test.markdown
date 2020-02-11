530454659110ccca3e71f71017868415cb92253b
Alexander Mussell <alexander.mussell@hotmail.co.uk>
2020-02-11 21:57:04 +0000

    search start

---
+++ _posts/2020-01-01-ansible-test.markdown
@@ -0,0 +1,7 @@
+---
+layout: post
+title:  "Ansible Test"
+date:   2020-01-01 21:03:36 +0530
+categories: Ansible
+---
+This is a search test
---
+++ assets/js/blog_search.js
@@ -0,0 +1,6 @@
+SimpleJekyllSearch({
+    searchInput: document.getElementById('search-input'),
+    resultsContainer: document.getElementById('results-container'),
+    json: '/search.json',
+  })
+  
