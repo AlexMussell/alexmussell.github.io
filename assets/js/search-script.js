4f65f5c0e05d8172a9fec55d578ba354cb1cad69
Alexander Mussell <alexander.mussell@hotmail.co.uk>
2020-02-13 19:25:19 +0000

    search working but rending template syntax

--- assets/js/search-script.js
+++ assets/js/search-script.js
@@ -1,6 +1,24 @@
 SimpleJekyllSearch({
   searchInput: document.getElementById('search-input'),
   resultsContainer: document.getElementById('results-container'),
-  searchResultTemplate: '<div><a href="{url}"><h1>{title}</h1></a><span>{date}</span></div>',
+  searchResultTemplate: '<ul class="base-layout"> \
+                            <li>{{date}} \
+                              <a class="post-link" href="{{url}}"> \
+                                <h2 class="post-title">{{title}}</h2> \
+                              </a> \
+                              <div class="post-meta"> \
+                                <ul class="post-categories"> \
+                                  <li>{{tags}}</li> \
+                                </ul> \
+                                <div class="post-date"> \
+                                  <i class="icon-calendar"></i> \
+                                  {{date}} \
+                                </div> \
+                              </div> \
+                            <div class="post"> \
+                              {{excerpt}} \
+                            </div> \
+                          </li> \
+                        </ul>',
   json: '/search.json',
 })
