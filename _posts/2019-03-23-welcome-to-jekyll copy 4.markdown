99b830fa118c5d6078ff80def56e11b7c4053f1e
Alexander Mussell <alexander.mussell@hotmail.co.uk>
2020-02-19 21:10:05 +0000

    search functionality finished. Needs styling

--- _posts/2019-03-23-welcome-to-jekyll copy 4.markdown
+++ _posts/2019-03-23-welcome-to-jekyll copy 4.markdown
@@ -6,10 +6,6 @@ category: Ansible
 ---
 test-2
 
-```python
-for x in range(10):
-	print(x)
-```
 
 Check out the [Jekyll docs][jekyll-docs] for more info on how to get the most out of Jekyll. File all bugs/feature requests at [Jekyll’s GitHub repo][jekyll-gh]. If you have questions, you can ask them on [Jekyll Talk][jekyll-talk].
 
--- assets/js/search.js
+++ assets/js/search.js
@@ -2,23 +2,19 @@
     function displaySearchResults(results, store) {
         var searchResults = document.getElementById('search-results');
 
-        console.log(results.length);
         // if (Object.keys(store).length == 0) {
         //     window.location.replace("/blog");
         // }
-        
+
         if (results.length) {
             var appendString = '';
 
-            const matched_posts =  Object.keys(store);
-
-            for (var i = 0; i < matched_posts.length; i++) {
-                const { title, category, url } = store[matched_posts[i]];
-                appendString += '<a href="' + url + '"><h3>' + title + '</h3><h3>' + category + '</h3></a>';
+            for (var i = 0; i < results.length; i++) {
+                var item = store[results[i].ref];
+                appendString += '<a href="' + item.url + '"><h3>' + item.title + '</h3><h3>' + item.category + '</h3></a>';
                 // appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
             }
-
-
+            
             searchResults.innerHTML = appendString;
         } else {
             searchResults.innerHTML = '<li>No results found</li>';
@@ -43,20 +39,17 @@
     if (searchTerm) {
         document.getElementById('search-box').setAttribute("value", searchTerm);
 
-        // Initalize lunr with the fields it will be searching on. I've given title
-        // a boost of 10 to indicate matches on this field are more important.
         var idx = lunr(function () {
-            this.ref('id')
+            this.field('id');
             this.field('title', { boost: 10 });
             this.field('category');
 
-            for (var key in window.store) { // Add the data to lunr
-                this.add({ id: 1, title: window.store[key].title }),
-                this.add({ id: 2, category: window.store[key].category })    
+            for (var key in window.store) {
+                this.add({ 'id': key, 'title': window.store[key].title, 'category': window.store[key].category })
             }
         });
-        
-        var results = idx.search(searchTerm); // Get lunr to perform a search
+
+        var results = idx.search(searchTerm);
         displaySearchResults(results, window.store);
 
     }
