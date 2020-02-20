76f2fbd7f932a1c4e3bf877ad3e7464df653dc8b
Alexander Mussell <alexander.mussell@hotmail.co.uk>
2020-02-20 19:33:56 +0000

    start dynamically building posts on search

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,16 +2,70 @@
     function displaySearchResults(results, store) {
         var searchResults = document.getElementById('search-results');
 
+        console.log(store);
+
         if (results.length) {
-            var appendString = '';
+            // var appendString = '';
+            var fragment = document.createDocumentFragment();
 
             for (var i = 0; i < results.length; i++) {
                 var item = store[results[i].ref];
-                appendString += '<a href="' + item.url + '"><h3>' + item.title + '</h3><h3>' + item.category + '</h3></a>';
+                // appendString += '<a href="' + item.url + '"><h3>' + item.title + '</h3><h3>' + item.category + '</h3></a>';
                 // appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+                
+                post = document.createElement('li');
+
+                titleLink = document.createElement('a');  
+                titleLink.classList.add("post-link");
+                titleLink.href = item.url
+
+                title = document.createElement('h2');
+                title.classList.add("post-title");
+                title.textContent = item.title
+
+                titleLink.appendChild(title)
+                post.appendChild(titleLink);
+
+                categoryDateContainer = document.createElement('div');
+                categoryDateContainer.classList.add('post-meta');
+
+                categories = document.createElement('ul');
+                categories.classList.add('post-categories');
+
+                categoryList = document.createElement('li');
+                categoryList.textContent = item.category;
+
+                categories.appendChild(categoryList);
+                categoryDateContainer.appendChild(categories);
+
+                date = document.createElement('div');
+                date.classList.add('post-date');
+
+                date.textContent = item.date;
+
+                dateIcon = document.createElement('i');
+                dateIcon.classList.add('icon-calendar');
+
+
+                date.appendChild(dateIcon);
+
+
+
+                categoryDateContainer.appendChild(date);
+
+
+                
+
+
+
+                post.appendChild(categoryDateContainer);
+
+
+                fragment.appendChild(post);
             }
 
-            searchResults.innerHTML = appendString;
+            //searchResults.innerHTML = appendString;
+            searchResults.appendChild(fragment);
         } else {
             searchResults.innerHTML = '<li>No results found</li>';
         }
@@ -43,9 +97,10 @@
             this.field('id');
             this.field('title', { boost: 10 });
             this.field('category');
+            this.field('date');
 
             for (var key in window.store) {
-                this.add({ 'id': key, 'title': window.store[key].title, 'category': window.store[key].category })
+                this.add({ 'id': key, 'title': window.store[key].title, 'category': window.store[key].category, 'date': window.store[key].date })
             }
         });
 
