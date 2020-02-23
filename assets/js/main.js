b20215ffb63f9cb42aa2d083e5d2db189a92a922
Alexander Mussell <alexander.mussell@hotmail.co.uk>
2020-02-23 12:11:44 +0000

    text of switch changes depending on mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,12 @@ function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
-        
+        document.getElementById('theme-text').textContent = "Dark Mode";
     }
     else {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
+        document.getElementById('theme-text').textContent = "Light Mode";
     }    
 }
 
